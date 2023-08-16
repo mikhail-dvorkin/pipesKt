22ce368d562eb9ae05592ebb4007cd42a8cf80b2
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-08-16 18:19:26 +0300

    Add help prompt

--- test_redirector.cmd
+++ test_redirector.cmd
@@ -1,4 +1,5 @@
 call gradlew build
 start java -cp build\libs\visualizer-1.0.0-all.jar RedirectorKt --showcase
+echo Redirector started. Input two lines there, it should receive their concatenation from A+B program.
 pause
 start java -cp build\libs\visualizer-1.0.0-all.jar Sample_aplusbKt
