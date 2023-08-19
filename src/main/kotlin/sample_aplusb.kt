dbbc0fb4bc2720c7974f44f71ec7fc3821bb08c9
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-08-19 16:30:22 +0300

    Fix even number of bugs :)

--- src/main/kotlin/sample_aplusb.kt
+++ src/main/kotlin/sample_aplusb.kt
@@ -2,10 +2,10 @@ import java.io.File
 
 fun main() {
 	val fileName = "redirector"
-	val inputFileName = "$fileName.out"
-	val outputFileName = "$fileName.in"
-	val input = File(getPipePrefix() + outputFileName).bufferedReader()
-	val output = File(getPipePrefix() + inputFileName).printWriter()
+	val inputFileName = "$fileName.in"
+	val outputFileName = "$fileName.out"
+	val input = File(getPipePrefix() + inputFileName).bufferedReader()
+	val output = File(getPipePrefix() + outputFileName).printWriter()
 	val a = input.readLine()
 	val b = input.readLine()
 	output.println(a + b)
